dc57f856b28051e48a72598384e7d69f309a003f
Christopher Barney <christopher.b.barney@gmail.com>
2023-11-20 23:20:06 -0800

    Version 0.0.1 - Initial Auto-update Testing

--- UI/Properties/AssemblyInfo.cs
+++ UI/Properties/AssemblyInfo.cs
@@ -1,7 +1,3 @@
-using System.Reflection;
 using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-
-
-[assembly: AssemblyMetadata("SquirrelAwareVersion", "1")]
